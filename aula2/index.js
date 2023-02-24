c12b403286d3fb731cfaa716343f0f84f4c7b5c6
Joao Sales <joaovictorlsales@gmail.com>
2023-02-24 20:19:45 -0300

    Maneiras de printar algo na tela

---
+++ aula2/index.js
@@ -0,0 +1,8 @@
+// Javascript - Tipagem dinâmica
+// String - Tipo de dado
+console.log('Joao "Sales"'); // String = Texto = Cadeia de caracteres
+console.log("Joao 'Sales'");
+console.log(`'Joao' "Sales"`); // Template String
+console.log(100, 15.85, 'Joao Sales De Novo');
+console.log(85); // Number = Número
+console.log(20.1234); // Number
