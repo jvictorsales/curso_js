26ecbad149bd0b432464e0b5f163ab812aa6141c
Joao Sales <joaovictorlsales@gmail.com>
2023-02-24 20:38:31 -0300

    Comentários

---
+++ aula3/index.js
@@ -0,0 +1,18 @@
+// Comentários
+// São trechos de código que são ignorados pelo motor(engine) do JavaScript
+// NodeJS utiliza a mesma engine(motor) do Google Chrome, V8.
+
+// Comentário de linha
+
+/**
+ * Comentário
+ * de
+ * Múltiplas
+ * Linhas
+ */
+
+console.log('Olá, mundo!');
+console.log('Linha 1');
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
